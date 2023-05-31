5bac57eda60fd61781c20e295656529244a568c8
Renzixuan <lily2ren@gmail.com>
2023-05-30 21:12:06 -0400

    convert to slash commands

--- markov.py
+++ markov.py
@@ -1,26 +1,30 @@
 # markov.py
 import numpy as np
+from cache import AsyncLRU
 
+@AsyncLRU(maxsize=128)
 async def get_ideal_rolls(start, desired_p):
     tmat = await get_markov_tmat(start)
 
-    init_progression = np.array([1 / start] * start)
+    init_progression = np.array([1 / start] * start, dtype=np.float16)
 
-    p = init_progression[-1]
+    p = [init_progression[-1]]
     roll_count = 0
 
-    while p < desired_p:
+    while p[-1] < 1 - desired_p:
         roll_count = roll_count + 1
 
         init_progression = np.matmul(init_progression, tmat)
-        p = init_progression[-1]
+        p.append(init_progression[-1])
 
-    return roll_count + 1
+    house_win_rate = 1 - p[-2]
+
+    return (roll_count + 1, house_win_rate)
 
 async def get_markov_tmat(n):
     mat = []
     for i in range(n):
-        row = [0] * (i) + [1/(n-i)] * (n-i)
+        row = np.array([0] * (i) + [1/(n-i)] * (n-i), dtype=np.float16)
         mat.append(row)
 
-    return np.array(mat)
+    return np.array(mat, dtype=np.float16)
